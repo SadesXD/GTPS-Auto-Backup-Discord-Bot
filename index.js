02a8553715493c233db5655073dcdd73ebaa3e2d
SadesXD <68439411+SadesXD@users.noreply.github.com>
2021-07-03 12:32:01 +0700

    Update index.js

--- index.js
+++ index.js
@@ -4,34 +4,30 @@
  */
 
 const Discord = require("discord.js");
-const client = new Discord.Client();
+const client = new Discord.Client({
+  restRequestTimeout: 120000,
+});
 const { prefix, token } = require("./config.json");
-const { existsSync } = require("fs")
+const { existsSync } = require("fs");
 
-const Backup = require('./main');
+const Backup = require("./main");
 const backup = new Backup(client, {
   config: require("./config.json"),
-  discord: Discord
+  discord: Discord,
 });
 
-// Utils
-const reply_backup = require("./Commands/backup");
-
 client.on("ready", async () => {
-  
   client.user.setActivity(`GTPS Auto Backup | ${prefix}help | By SadesXD#3971`, {
-    type:"WATCHING"
+    type: "WATCHING",
   });
-  
+
   console.log(`${client.user.tag} is ready now`);
   backup.send_backup();
 });
 
 client.on("message", async (message) => {
   if (message.author.bot) return;
-
-  reply_backup.send_msg(message);
-
+  if (message.channel.type == "dm") return;
   if (!message.content.startsWith(prefix)) return;
 
   let args = message.content.slice(prefix.length).trim().split(" ");
@@ -48,4 +44,4 @@ client.on("message", async (message) => {
   }
 });
 
-client.login(token)
+client.login(token);
