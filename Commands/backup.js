a6766b49d75b705d3f423444aacf65d32cf686ea
SadesXD <68439411+SadesXD@users.noreply.github.com>
2021-07-03 12:33:13 +0700

    Update backup.js

--- Commands/backup.js
+++ Commands/backup.js
@@ -4,86 +4,70 @@ const Backup = new (require("../main.js"))(new Discord.Client(), {
   discord: Discord,
   config: config,
 });
-const reply = require("../Data/Data.json");
 const { stripIndent } = require("common-tags");
 
-module.exports = {
-  run: async (client, message, args) => {
-    // If user doesn't have specified role (role id in config.json file), so the user can't use this command
-    if (!message.member.roles.cache.has(config.role_id)) {
-      return message.channel.send("You don't have any role permission !");
-    }
+exports.run = async (client, message, args) => {
+  if (!message.member.roles.cache.has(config.role_id)) {
+    return message.channel.send(
+      `${message.author} You don't have any role permissions for using this command`
+    );
+  }
 
-    const embed = new Discord.MessageEmbed()
-      .setAuthor(message.author.tag, message.author.displayAvatarURL())
-      .setColor("RANDOM")
-      .setDescription(
-        `\`\`\`${stripIndent`
-        Are you sure want to backup on this channel?
-        ---
-        You must be sure that you will back up the server on this channel | be careful of other people who will take your files
-        ---
-        Type ( yes | y ) if you want | Type (no | n) if you don't want
-        `}\`\`\``
-      )
-      .setTimestamp();
+  let desc = stripIndent(`
+  Are you sure want to backup on this channel?
+  ---
+  You must be sure that you will back up the server on this channel | be careful of other people who will take your files
+  `);
 
-    let result_msg = await message.channel.send(embed);
-    reply[message.author.id] = 1;
-    setTimeout(() => {
-      if (!reply[message.author.id]) return;
-      embed.setDescription("```Canceled```");
-      result_msg.edit(embed);
-      delete reply[message.author.id];
-    }, 10000);
-  },
-  send_msg: (message) => {
-    if (reply[message.author.id]) {
-      let userMsg = message.content.toLowerCase();
-      if (userMsg === "yes" || userMsg === "y") {
-        let msg = {
-          embed: {
-            color: "RANDOM",
-            fields: [
-              {
-                name: "Server stats",
-                value: `\`\`\`${stripIndent`
-                            World Created Count: ${
-                              Backup.get_all_files(config.world_folder).length
-                            }
-                            World Size: ${Backup.get_total_size(
-                              config.world_folder
-                            )}
-                            Player Registered Count: ${
-                              Backup.get_all_files(config.player_folder).length
-                            }
-                            Player Size: ${Backup.get_total_size(
-                              config.player_folder
-                            )}
-                            `}\`\`\``,
-                inline: true,
-              },
-            ],
-          },
+  const embed = new Discord.MessageEmbed()
+    .setAuthor(message.author.tag, message.author.displayAvatarURL())
+    .setColor("RANDOM")
+    .setDescription("```" + desc + "```")
+    .setTimestamp();
+
+  message.channel.send(embed).then((msg) => {
+    msg.react("✅");
+    msg.react("❎");
+
+    let correctFilter = (r, u) => r.emoji.name === "✅" && u.id === message.author.id;
+    let wrongFilter = (r, u) => r.emoji.name === "❎" && u.id === message.author.id;
+
+    let correctCollect = msg.createReactionCollector(correctFilter, { time: 10000 });
+    let wrongCollect = msg.createReactionCollector(wrongFilter, { time: 10000 });
+
+    correctCollect
+      .on("collect", async (c) => {
+        let dsc = stripIndent(`
+          World Created Count: ${Backup.get_all_files(config.world_folder).length}
+          World Size: ${Backup.get_total_size(config.world_folder)}
+          Player Created Count: ${Backup.get_all_files(config.player_folder).length}
+          Player Size: ${Backup.get_total_size(config.player_folder)}
+        `);
+
+        Backup.backup_file();
+        Backup.save_data(Date.now());
+
+        message.channel.send("Please wait...");
+        embed.setDescription("```" + dsc + "```");
+        message.author.send({
+          embed,
           files: [
             {
               attachment: "./GTPS_Backup.zip",
               name: "Backup-result.zip",
             },
           ],
-        };
+        });
+        correctCollect.stop();
+      })
+      .on("end", async (x) => {
+        correctCollect.stop();
+      });
 
-        Backup.backup_file();
-        Backup.save_data(Date.now());
-        message.author
-          .send("```Backup Loaded```", msg)
-          .then(() => message.channel.send("Check your dm"))
-          .catch(() => message.channel.send("```Backup Loaded```", msg));
-        delete reply[message.author.id];
-      } else if (userMsg === "no" || userMsg === "n") {
-        delete reply[message.author.id];
-        return message.channel.send("Canceled");
-      }
-    }
-  },
+    wrongCollect.on("collect", async (x) => {
+      embed.setDescription("Canceled !");
+      msg.edit({ embed });
+      correctCollect.stop();
+    });
+  });
 };
