c8de5a39b52ef82e797bad37fbb51c0ab6c2e61c
SadesXD <68439411+SadesXD@users.noreply.github.com>
2021-07-03 12:34:53 +0700

    Update show.js

--- Commands/show.js
+++ Commands/show.js
@@ -10,22 +10,17 @@ const Backup = new (require("../main"))(new Discord.Client(), {
 
 exports.run = async (client, message, args) => {
   let get_data = Data.times || Data.time;
+  let dsc = stripIndent(`
+  World Created Count: ${Backup.get_all_files(config.world_folder).length}
+  World Size: ${Backup.get_total_size(config.world_folder)}
+  Player Registered Count: ${Backup.get_all_files(config.player_folder).length}
+  Player Size: ${Backup.get_total_size(config.player_folder)}
+  `);
+
   const embed = new Discord.MessageEmbed()
     .setAuthor("Server Statistic")
     .setColor("RANDOM")
-    .addField(
-      "Stats: ",
-      `\`\`\`${stripIndent`
-    World Created Count: ${Backup.get_all_files(config.world_folder).length}
-    World Size: ${Backup.get_total_size(config.world_folder)}
-    Player Registered Count: ${
-      Backup.get_all_files(config.player_folder).length
-    }
-    Player Size: ${Backup.get_total_size(config.player_folder)}
-    `}\`\`\``
-    )
-    .setFooter(
-      `Last Backup: ${moment.utc(get_data).format("lll")} ( UTC-Time )`
-    );
+    .addField("Stats: ", `\`\`\`${dsc}\`\`\``)
+    .setFooter(`Last Backup: ${moment.utc(get_data).format("lll")} ( UTC-Time )`);
   return message.channel.send(embed);
 };
