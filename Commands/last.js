a5dd7432583289d49373ab647e9be8118f5c887d
SadesXD <68439411+SadesXD@users.noreply.github.com>
2021-07-03 12:45:14 +0700

    Update last.js

--- Commands/last.js
+++ Commands/last.js
@@ -3,15 +3,17 @@ const Data = require("../Data/Data.json");
 const moment = require("moment");
 const ms = require("ms");
 
-exports.run = async(client, message, args) => {
-
-    let get_data = Data.times || Data.time;
-    const embed = new discord.MessageEmbed()
+exports.run = async (client, message, args) => {
+  let get_data = Data.times || Data.time;
+  if (!get_data) {
+    return message.channel.send("You don't have a backup of your server !");
+  }
+  const embed = new discord.MessageEmbed()
     .setAuthor("Last backup time")
     .setColor("RANDOM")
-    .addField('Last time (UTC-Time)', `\`\`\`${moment.utc(get_data).format('lll')}\`\`\``,true)
+    .addField("Last time (UTC-Time)", `\`\`\`${moment.utc(get_data).format("lll")}\`\`\``, true)
     .addField("Time now (UTC-Time)", `\`\`\`${moment.utc(Date.now()).format("lll")}\`\`\``, true)
     .addField("Since", `\`\`\`${ms(Date.now() - get_data)} Ago\`\`\``)
-    .setTimestamp()
-    return message.channel.send(embed);
-}
+    .setTimestamp();
+  return message.channel.send(embed);
+};
