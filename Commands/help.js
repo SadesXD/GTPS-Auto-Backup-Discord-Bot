49233e305a5c29e93ec816cff82fc6953282c78f
SadesXD <68439411+SadesXD@users.noreply.github.com>
2021-07-26 16:42:58 +0700

    Update help.js

--- Commands/help.js
+++ Commands/help.js
@@ -1,20 +1,15 @@
-const Discord = require("discord.js");
+const discord = require("discord.js");
 const { readdirSync } = require("fs");
+const seperate = ", "
 
-exports.run = async(client, message, args) => {
-    let Commands = get_all_command();
-    let command = [...Commands].map(x => x = `\`${x}\``).join(", ");
-
-    const embed = new Discord.MessageEmbed()
-    .setAuthor('Commands List')
-    .addField(`Commands[${Commands.length}]`, command, true)
+exports.run = (client, message, args) => {
+    let cmd = readdirSync("./Commands")
+    let command = cmd.map(x => x = "`" + x.split(".")[0] + "`").join(seperate);
+    const embed = new discord.MessageEmbed()
+    .setAuthor("Command list")
     .setColor("RANDOM")
+    .addField(`Commands [${cmd.length}]`, command, true)
     .setTimestamp()
-    return message.channel.send(embed);
-    
-};
 
-function get_all_command() {
-    let command = readdirSync("./Commands");
-    return command.map(x => x.split(".")[0]);
+    return message.channel.send(embed);
 }
