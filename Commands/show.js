53efe9931ce609f98a151efb0e13c399fd340551
SadesXD <68439411+SadesXD@users.noreply.github.com>
2021-07-26 16:45:15 +0700

    Update show.js

--- Commands/show.js
+++ Commands/show.js
@@ -2,27 +2,25 @@ const Discord = require("discord.js");
 const config = require("../config.json");
 const moment = require("moment");
 const { stripIndent } = require("common-tags");
-const Data = require("../Data/Data.json");
-const Backup = new (require("../main"))(new Discord.Client(), {
-  config: config,
-  discord: Discord,
-});
+const data = require("../Data/data.json");
+const { getAllFiles, getTotalSize } = require("../main.js")
 
-exports.run = async (client, message, args) => {
-  let get_data = Data.times || Data.time;
-  let dsc = stripIndent(`
-  World Created Count: ${Backup.get_all_files(config.world_folder).length}
-  World Size: ${Backup.get_total_size(config.world_folder)}
-  Player Registered Count: ${Backup.get_all_files(config.player_folder).length}
-  Player Size: ${Backup.get_total_size(config.player_folder)}
-  `);
+exports.run = (client, message, args) => {
+    let get_data = data.time;
+    get_data = get_data ? `${moment.utc(data.time).format("lll")} (UTC Time)` : "None";
+    let dsc = stripIndent(`
+    World Created Count: ${getAllFiles(config.gtps_folder + config.world_folder).length}
+    World Size: ${getTotalSize(config.gtps_folder + config.world_folder)}
+    Player Created Count: ${getAllFiles(config.gtps_folder + config.player_folder).length}
+    Player Size: ${getTotalSize(config.gtps_folder + config.player_folder)}
+    GTPS Size: ${getTotalSize(config.gtps_folder)}
+    `)
 
-  const embed = new Discord.MessageEmbed()
+    const embed = new Discord.MessageEmbed()
     .setAuthor("Server Statistic")
     .setColor("RANDOM")
-    .addField("Stats: ", `\`\`\`${dsc}\`\`\``)
-    .setFooter(
-      `Last Backup: ${get_data ? moment.utc(get_data).format("lll") + " ( UTC-Time )" : "None"}`
-    );
-  return message.channel.send(embed);
-};
+    .addField("Stats", "```" + dsc + "```", true)
+    .setFooter("Last Backup: " + get_data)
+
+    return message.channel.send(embed);
+}
