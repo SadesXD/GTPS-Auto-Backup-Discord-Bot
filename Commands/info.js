fdb9f8e17f881be6f3f49f9a44dea7874066d188
SadesXD <68439411+SadesXD@users.noreply.github.com>
2021-07-26 16:43:35 +0700

    Update info.js

--- Commands/info.js
+++ Commands/info.js
@@ -1,37 +1,38 @@
 const Discord = require("discord.js");
-const { stripIndent } = require("common-tags");
-const moment = require("moment");
 const cpu = require("cpu-stat");
 const ms = require("ms");
-const { convert_bytes } = new (require("../main"))(new Discord.Client(), {
-  discord: Discord,
-  config: require("../config.json"),
-});
+const moment = require("moment");
+const { stripIndent } = require("common-tags")
+const { convertBytes } = require("../main.js");
 
-exports.run = async (client, message, args) => {
-  cpu.usagePercent((error, percents, seconds) => {
-    let bs = stripIndent(`
+exports.run = (client, message, args) => {
+
+    cpu.usagePercent((error, percents, seconds) => {
+
+        let bs = stripIndent(`
         Bot Uptime: ${ms(client.uptime)}
         Bot Created: ${moment.utc(client.user.createdAt).format("ll")}
         Discord.js Version: ${require("../package.json").dependencies["discord.js"] || "n/a"}
         `);
 
-    let ss = stripIndent(`
-        Memory Usage: ${convert_bytes(process.memoryUsage().heapUsed)} 
+        let ss = stripIndent(`
+        Memory Usage: ${convertBytes(process.memoryUsage().heapUsed)} 
         CPU Usage: ${percents.toFixed(2)} %
         Node.js Version: ${process.version}
         `);
 
-    const embed = new Discord.MessageEmbed()
+        const embed = new Discord.MessageEmbed()
       .setAuthor("GTPS Auto Backup Discord Bot")
       .setColor("RANDOM")
       .addField("Bot Stats: ", "```" + bs + "```", true)
       .addField("System Stats: ", "```" + ss + "```", true)
       .addField(
         "Bot Github (Open Source): ",
-        "[Source](https://github.com/SadesXD/GTPS-Auto-Backup-Discord-Bot)"
-      )
+        "[Source](https://github.com/SadesXD/GTPS-Auto-Backup-Discord-Bot)")
       .setTimestamp();
-    return message.channel.send(embed);
-  });
-};
+        
+      return message.channel.send(embed);
+
+    })
+
+}
