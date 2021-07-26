9a746553dffcdfde9e15bfd7ba251175607b981e
SadesXD <68439411+SadesXD@users.noreply.github.com>
2021-07-26 16:50:01 +0700

    Update install.bat

--- Build/install.bat
+++ Build/install.bat
@@ -1,6 +1,4 @@
 @echo off
-echo install the requirement package
+where node.exe >nul 2>&1 || echo [ERROR] Please install node js first before using this App && exit /B
+echo [INFO] Install the requirement package !..
 npm install && npm audit fix
-cls
-echo Done..
-pause
